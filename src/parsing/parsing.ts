0f597ce3eb16179db57a8ba43d93f9f18cdb6001
Brice GUILLAUME <brice.guillaume@algonomia.com>
2024-10-21 23:09:00 +0200

    [DEV-000] null parsing fix

--- src/parsing/parsing.ts
+++ src/parsing/parsing.ts
@@ -6,46 +6,55 @@ export function getValueFromKeyIfTypeCompatibleMandatory<T>(key: string, jsonObj
 }
 
 export function getValueFromKeyIfTypeCompatible<T>(key: string, jsonObject: {[key: string]: any}, type: any, isArray: boolean = false): T|undefined {
-    if (jsonObject[key] === undefined) {
+    let jsonValue = jsonObject[key];
+    if (jsonValue === undefined) {
         return undefined;
+    }
+    if (jsonValue === null) {
+        return null;
     }
     if (isArray) {
         try {
-            return jsonObject[key].map((iteration: any) => getValueFromKeyIfTypeCompatible<T>('key', {key: iteration}, type));
+            return jsonValue.map((iteration: any) => getValueFromKeyIfTypeCompatible<T>('key', {key: iteration}, type));
         } catch (e) {
             throw e;
         }
+    } else if (jsonValue instanceof Array) {
+        if (jsonValue.length > 1) {
+            throw new Error('Wrong type for key "' + key + '", is an array but should not be.');
+        }
+        jsonValue = jsonValue[0];
     }
     if (typeof type !== 'function') {
-        if (typeof jsonObject[key] !== type) {
+        if (typeof jsonValue !== type) {
             try {
                 if (type === 'number') {
-                    const ret = Number(jsonObject[key]);
+                    const ret = Number(jsonValue);
                     if (isNaN(ret)) { throw new Error(); }
                     return <T><unknown>ret;
                 }
                 if (type === 'boolean') {
-                    const ret = Boolean(JSON.parse(typeof jsonObject[key] === 'string' ? jsonObject[key].toLowerCase() : jsonObject[key]));
+                    const ret = Boolean(JSON.parse(typeof jsonValue === 'string' ? jsonValue.toLowerCase() : jsonValue));
                     return <T><unknown>ret;
                 }
                 if (typeof type === 'object') {
                     if (Object.values(type).slice(Object.values(type).length / 2).every(value => typeof value === 'number')) {
-                        const ret = Number(jsonObject[key]);
+                        const ret = Number(jsonValue);
                         if (isNaN(ret)) { throw new Error(); }
                         return <T><unknown>ret;
                     }
                 }
                 if (type === 'string') {
-                    return <T><unknown>String(jsonObject[key]);
+                    return <T><unknown>String(jsonValue);
                 }
             } catch (e) {
                 throw new Error('Wrong type for key "' + key + '", should be: ' + type);
             }
         }
-    } else if (!(jsonObject[key] instanceof type)) {
+    } else if (!(jsonValue instanceof type)) {
         try {
             if (type === Date) {
-                let val = jsonObject[key];
+                let val = jsonValue;
                 if (!isNaN(Number(val))) {
                     val = Number(val);
                 }
@@ -57,5 +66,5 @@ export function getValueFromKeyIfTypeCompatible<T>(key: string, jsonObject: {[ke
             throw new Error('Wrong type for key "' + key + '", should be: ' + type);
         }
     }
-    return <T>jsonObject[key];
+    return <T>jsonValue;
 }
